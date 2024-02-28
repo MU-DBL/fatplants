7fd9a38151b4bd790bba2aad6f9580af317d120c
naveed <45944298+Naveed319@users.noreply.github.com>
2024-02-28 10:24:42 -0600

    modified routing file

--- fatplant/src/app/app-routing.module.ts
+++ fatplant/src/app/app-routing.module.ts
@@ -33,6 +33,7 @@ import { ProteinCamelinaComponent } from './components/pages/datapages/protein-c
 import { ExtendedPathwayComponent } from './components/pages/tools/extended-pathway/extended-pathway.component';
 import { BlastInternalComponent } from './components/pages/tools/blast-internal/blast-internal.component';
 import { ProteinDetailsNewComponent } from './components/pages/datapages/protein-details-new/protein-details-new.component';
+import {DatasetsNewComponent} from './components/pages/datapages/datasets-new/datasets-new.component';
 
 const routes: Routes = [{path: '', redirectTo: '/homepage', pathMatch: 'full'},
 {path: 'homepage', component: HomepageComponent},
@@ -59,11 +60,12 @@ const routes: Routes = [{path: '', redirectTo: '/homepage', pathMatch: 'full'},
 {path: 'soybean', component: SoybeanComponent},
 {path: 'add-news', component: AddNewsComponent},
 {path: 'datasets', redirectTo: '/datasets/arabidopsis', pathMatch: 'full'},
-{path: 'datasets/:dataset', component: UnifiedDatapageComponent},
+{path: 'datasets/:dataset', component: UnifiedDatapageComponent}, //component: DatasetsNewComponent
 {path: 'showresults/:uniprot_id/:cfg', component: ShowresultsComponent},
 {path: 'protein/:uniprot_id', component: ProteinDetailComponent},
-{path: 'soybean_prot/:uniprot_id', component: ProteinDetailsNewComponent}, //component: ProteinSoybeanComponent
+{path: 'soybean_prot/:uniprot_id', component: ProteinSoybeanComponent}, 
 {path: 'camelina_prot/:uniprot_id', component: ProteinCamelinaComponent},
+// {path: 'details/:database_name/:uniprot_id',component:ProteinDetailsNewComponent},
 {path: 'pathway-viewer', component: PathwayViewerComponent},
 {path: 'extended-pathway', component: ExtendedPathwayComponent},
 {path: 'blast-internal', component:BlastInternalComponent},
