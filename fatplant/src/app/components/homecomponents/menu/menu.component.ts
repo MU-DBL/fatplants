799d7872b015028aa7f26e502c41d8d198d24099
Harshavardhan Chintapatla <chintapatlaharshavardhan@gmail.com>
2024-05-13 13:48:18 -0500

    Fix for menu page getting disappeared

--- fatplant/src/app/components/homecomponents/menu/menu.component.ts
+++ fatplant/src/app/components/homecomponents/menu/menu.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  public isSidebarVisible: boolean = false;
+  public showHamburgerIcon: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public toggleSidebar(): void {
+    this.isSidebarVisible = !this.isSidebarVisible;
+    this.showHamburgerIcon = !this.isSidebarVisible;
+  }
+  public closeSidebar(): void {
+    this.isSidebarVisible = false;
+    this.showHamburgerIcon = true;
+  }
 }
