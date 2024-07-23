c488f27854ea55e0f9e313197c5fad6f59e62794
gcycaas <gcycaas@gmail.com>
2024-07-23 16:32:02 -0500

    Update counter.component.ts

--- fatplant/src/app/components/counter/counter.component.ts
+++ fatplant/src/app/components/counter/counter.component.ts
@@ -12,7 +12,6 @@ export class CounterComponent implements OnInit {
   clientIp: string | null = null;
   randomNumber: number;
   count: string | null = "";
-  private countRes = new Observable<string>();
 
   constructor(private http: HttpClient) { }
 
@@ -25,28 +24,15 @@ export class CounterComponent implements OnInit {
     this.http.get<{ ip: string }>('https://api.ipify.org/?format=json')
       .subscribe(response => {
         this.clientIp = response.ip;
-        console.log(this.clientIp)
-        /*
-        this.countRes=this.http.get(environment.BASE_API_URL+"visit/?info="+this.clientIp, { responseType: 'text' });//.toString().padStart(6, '0');
-        this.countRes.subscribe(res=>{
-          console.log(res)
-          this.count = res;
-          console.log(this.clientIp)
-        })
-          */
+        //console.log(this.clientIp)
         this.http.get<{ res: string }>(environment.BASE_API_URL+"visit/?info="+this.clientIp)
           .subscribe(response => {
-            console.log(response)
+            //console.log(response)
             this.count = response.toString().padStart(6, '0');
           })
       }, error => {
-        console.error('Error fetching IP address:', error);
+        //console.error('Error fetching IP address:', error);
         this.clientIp = 'Unable to fetch IP address';
       });
   }
-
-  generateRandomNumber(): void {
-    this.randomNumber = Math.floor(Math.random() * 100) + 1; // Generates a random number between 1 and 100
-  }
-
 }
